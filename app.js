364d47f115343acafea26a76e73f83e279e09174
Jonathan Doliver <porygonj@gmail.com>
2016-12-29 02:37:57 +0000

    basic app route layout

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 var express = require("express");
+var mongo = require("mongodb").MongoClient;
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/new/:url', function(req, res){
+    mongo.connect()
+    req.params.url 
+});
+
+app.get('/:id', function(req, res){
+    
+});
+
+app.listen(port, function(){
+    console.log("Listening on port " + port);
+});
