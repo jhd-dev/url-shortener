69d5a794c01df7a4fed55c9a68f801aabb074fa8
Jonathan Doliver <porygonj@gmail.com>
2016-12-29 23:38:34 +0000

    attempting mongodb connection

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var validUrl = require("valid-url");
 
 var app = express();
 var port = process.env.PORT || 8080;
+var mongoPort = 27017;
 var appURL = 'https://porygonj-url-shortener.herokuapp.com/';
 var notFoundPage = '';
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/new/:url', function(req, res){
-    mongo.connect('mongodb:localhost:' + port + '/urls', function(err, db){
+    mongo.connect('mongodb:localhost:' + mongoPort + '/urls', function(err, db){
         if (err) throw err;
         var url = req.params.url;
         var urls = db.collection('urls');
