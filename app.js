73fbbd19a3f146dc52ef09ed376ded3bf861f7cf
Jonathan Doliver <porygonj@gmail.com>
2016-12-29 13:06:37 +0000

    original url request logic

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ var port = process.env.PORT || 8080;
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/new/:url', function(req, res){
-    mongo.connect()
-    req.params.url 
+    mongo.connect('mongodb:localhost:' + port + '/urls', function(err, db){
+        if (err) throw err;
+        var urls = db.collection('urls');
+        urls.count({}, function(err, count){
+            if (err) throw err;
+            var toInsert = {
+                "original_url": req.params.url,
+                "new_url": count
+            };
+            urls.insert(toInsert, function(err, data){
+                if (err) throw err;
+            });
+        });
+    });
 });
 
 app.get('/:id', function(req, res){
